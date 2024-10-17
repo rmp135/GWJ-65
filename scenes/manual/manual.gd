af8bdee414f12ef69051c801d71c6da3c55188e1
Ryan Poole <rmp135@gmail.com>
2024-10-17 22:15:36 +0100

    Slight modification to how the manual is generated.

--- scenes/manual/manual.gd
+++ scenes/manual/manual.gd
@@ -5,41 +5,62 @@ class_name Manual
 @export var sign_in_manager : SignInManager
 @export var flip_sounds : Array[AudioStreamWAV] = []
 
-@onready var text: Label = %Instructions
+@onready var text_label: RichTextLabel = %RichTextLabel
 @onready var previous_button: Button = %PreviousButton
 @onready var next_button: Button = %NextButton
 @onready var instructions_rect: NinePatchRect = %InstructionsRect
 @onready var cover_rect: NinePatchRect = %CoverRect
 
+var pages: Array[String] = []
+
 var page := 0
-var max_pages := 5
 
 func _ready() -> void:
+	text_label.text += fault_manager.get_manual()
+	text_label.text += "\n\n"
+	text_label.text += sign_in_manager.get_manual()
+	split_content_into_pages()
 	previous_button.hide()
 	cover_rect.show()
 	instructions_rect.hide()
-	text.text += fault_manager.get_manual()
-	text.text += "\n\n"
-	text.text += sign_in_manager.get_manual()
-	max_pages = ceili(text.get_line_count() / 25) + 1
-	text.max_lines_visible = 25
-	
+
 func _on_button_pressed() -> void:
 	page = page + 1
 	update_page()
 
 func _on_previous_button_pressed() -> void:
 	if page > 0:
-		page = page - 1
+		page -= 1
 		update_page()
 
+func split_content_into_pages():
+	var temp_label := text_label.duplicate()
+	temp_label.bbcode_enabled = true
+	temp_label.size = text_label.size
+	add_child(temp_label)
+
+	var lines := text_label.text.split("\n")
+	var current_page_content := ""
+
+	for line in lines:
+		temp_label.bbcode_text = current_page_content + line + "\n"
+		if temp_label.get_content_height() > text_label.size.y:
+			pages.append(current_page_content)
+			current_page_content = line + "\n"
+		else:
+			current_page_content += line + "\n"
+
+	if current_page_content:
+		pages.append(current_page_content)
+
+	remove_child(temp_label)
+	temp_label.queue_free()
+
 func update_page() -> void:
-	previous_button.visible = page != 0
-	instructions_rect.visible = page != 0
 	cover_rect.visible = page == 0
-	
-	text.lines_skipped = (page - 1) * 25
-	next_button.visible = page < max_pages
+	text_label.bbcode_text = pages[page]
+	previous_button.visible = page != 0
+	instructions_rect.visible = page != pages.size() - 1
 	_play_flip()
 
 func _play_flip():
